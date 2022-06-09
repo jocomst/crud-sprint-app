d842fb900b385311535a046d67c189d571d39019
John <johncomstock9716@gmail.com>
2022-06-09 09:54:38 -0400

    added user and post entities

---
+++ blog/src/main/java/com/example/blog/repository/AuthorRepository.java
@@ -0,0 +1,12 @@
+package com.example.blog.repository;
+
+import com.example.blog.model.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface AuthorRepository extends JpaRepository<Author, Long> {
+    Optional<Author> findByUsername(String username);
+}
