fd21c3ea4763394d8123173620ce53ea1260bbf6
Choua Vang <c_vang4@outlook.com>
2022-06-10 10:50:44 -0400

    updated logout message and added login page

--- blog/src/main/java/com/example/blog/Controller/HomeController.java
+++ blog/src/main/java/com/example/blog/Controller/HomeController.java
@@ -1,11 +1,24 @@
 package com.example.blog.Controller;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
     public class HomeController {
+
         @RequestMapping("/")
         public String home() {
             return "home";
         }
+
+        @GetMapping("/logout")
+        public String logout() {
+            return "logout";
+        }
+
+        @GetMapping("/login")
+        public String login() {
+            return "login";
+        }
+
     }
