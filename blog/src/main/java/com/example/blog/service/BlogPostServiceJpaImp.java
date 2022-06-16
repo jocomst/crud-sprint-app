5b86a9735b36ba1688c47dce1cc935247fc97eff
John <johncomstock9716@gmail.com>
2022-06-16 13:10:42 -0400

    save

--- blog/src/main/java/com/example/blog/service/BlogPostServiceJpaImp.java
+++ blog/src/main/java/com/example/blog/service/BlogPostServiceJpaImp.java
@@ -17,44 +17,46 @@ import java.util.Optional;
 public class BlogPostServiceJpaImp implements BlogPostService {
 
 
-        @Autowired
-        private BlogPostRepository postRepo;
+    @Autowired
+    private BlogPostRepository postRepo;
+
     @Override
     public List<BlogPost> getAllBlogPosts() {
         return postRepo.findAll();
     }
+
     @Override
-        public List<BlogPost> findAll() {
+    public List<BlogPost> findAll() {
 
         return this.postRepo.findAll();
-        }
+    }
 
-        @Override
-        public List<BlogPost> findLatest5() {
+    @Override
+    public List<BlogPost> findLatest5() {
 
         return this.postRepo.findLatest5();
-        }
+    }
 
 //        @Override
 //        public BlogPost findById(Long id) {
 //            return null;
 //        }
 
-        @Override
-        public BlogPost findById(Long id) {
-            Optional<BlogPost> blog = this.postRepo.findById(id);
-            BlogPost blogPost = null;
-            if (blog.isPresent()){
-                blogPost = blog.get();
-            } else
-                throw new RuntimeException("BlogPost not found");
-            return blogPost;
-        }
+    @Override
+    public BlogPost findById(Long id) {
+        Optional<BlogPost> blog = this.postRepo.findById(id);
+        BlogPost blogPost = null;
+        if (blog.isPresent()) {
+            blogPost = blog.get();
+        } else
+            throw new RuntimeException("BlogPost not found");
+        return blogPost;
+    }
 
     @Override
     public void create(BlogPost post) {
 
-       postRepo.save(post);
+        postRepo.save(post);
     }
 
     @Override
